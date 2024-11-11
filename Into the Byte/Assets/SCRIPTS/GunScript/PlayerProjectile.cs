1127c2cc887e0a86781dd30ad1387f590bf0b552
SilentLight <andreijara27@gmail.com>
2024-11-12 04:25:45 +0800

    fixed malware visual bug

--- Into the Byte/Assets/SCRIPTS/GunScript/PlayerProjectile.cs
+++ Into the Byte/Assets/SCRIPTS/GunScript/PlayerProjectile.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerProjectile : MonoBehaviour
 {
-    public float speed = 10f;                // Speed of the projectile
+    private float speed = 80f;                // Speed of the projectile
     public float lifetime = 2f;              // How long the projectile exists before being destroyed
     public float damage = 10f;               // Damage dealt by the projectile
     public Vector2 direction = Vector2.right; // Default direction of the projectile (can be modified when instantiated)
